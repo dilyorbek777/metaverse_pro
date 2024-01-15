ed14ba7370eb388a12e8cdea4ffa24525af2b7fb
dilyorbek777 <asfandiyorovdilyorbek@gmail.com>
2024-01-15 22:53:35 +0500

    full projects

---
+++ components/Navbar.jsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div
+      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+    >
+
+      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+        METAVERSUS
+      </h2>
+      <a href='https://t.me/developer_23_05' className='text-lime-100 font-black'>by Developer</a>
+
+    </div>
+  </motion.nav>
+);
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Metaversus</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://stijndv.com" />
+      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
+
+export default MyApp;
